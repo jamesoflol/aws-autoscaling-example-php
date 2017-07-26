3346bb3e5201de007dd9e32647873fe93d43f823
James <james@hale.com.au>
2017-07-26 15:29:51 +1000

    Removed extra stuff - just the cpu stress loop

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 $time = microtime();
 $time = explode(' ', $time);
@@ -10,9 +11,7 @@ $start = $time;
 <body>
 <div align="center">
 
-<!--
-<h1 style="font-family:verdana;">Simple AWS test Page</h1>
--->
+<h1 style="font-family:verdana;">CPU Stress test Page</h1>
 
 <p style="font-family:verdana;font-size:130%;color:black">
 <?php
@@ -30,43 +29,13 @@ echo '<p style="font-family:verdana;font-size:180%;color:'.$COLOR.'">'.$EC2_AZ;
 ?>
 </p>
 
-<p style="font-family:verdana;font-size:90%;color:black">
-From EC2: 
-</p>
-
- <img src="leonardo1.jpg" width="120" height="120">
- <img src="leonardo2.jpg" width="120" height="120">
- <img src="leonardo3.jpg" width="120" height="120">
- <img src="leonardo4.jpg" width="120" height="120">
- <img src="leonardo5.jpg" width="120" height="120">
-
-<p style="font-family:verdana;font-size:90%;color:black">
-From Amazon S3: 
-</p>
-
- <img src="https://s3-us-west-2.amazonaws.com/simone-us-west-2/leonardo1.jpg" width="120" height="120">
- <img src="https://s3-us-west-2.amazonaws.com/simone-us-west-2/leonardo2.jpg" width="120" height="120">
- <img src="https://s3-us-west-2.amazonaws.com/simone-us-west-2/leonardo3.jpg" width="120" height="120">
- <img src="https://s3-us-west-2.amazonaws.com/simone-us-west-2/leonardo4.jpg" width="120" height="120">
- <img src="https://s3-us-west-2.amazonaws.com/simone-us-west-2/leonardo5.jpg" width="120" height="120">
-
-<p style="font-family:verdana;font-size:90%;color:black">
-From Amazon CloudFront: 
-</p>
-
- <img src="http://d2gvd8cg7m7q4d.cloudfront.net/leonardo1.jpg" width="120" height="120">
- <img src="http://d2gvd8cg7m7q4d.cloudfront.net/leonardo2.jpg" width="120" height="120">
- <img src="http://d2gvd8cg7m7q4d.cloudfront.net/leonardo3.jpg" width="120" height="120">
- <img src="http://d2gvd8cg7m7q4d.cloudfront.net/leonardo4.jpg" width="120" height="120">
- <img src="http://d2gvd8cg7m7q4d.cloudfront.net/leonardo5.jpg" width="120" height="120">
-
 <br><br><br>
 <p style="font-family:verdana;font-size:120%;color:black">
 CPU Stress test performed.
 </p>
 <?php
 // CPU stress test
-for($i = 0; $i < 10000000; $i++) {
+for($i = 0; $i < 100000000; $i++) {
      $a += $i;
 }
 ?>
@@ -85,3 +54,4 @@ echo '</p>';
 </div>
 </body>
 </html>
+
